cf420207abd8306caf595761585df014761978ed
anthrapper <jithuchy777@gmail.com>
2020-08-19 20:19:52 +0530

    fixed future builder firing

--- lib/UI/screens/Appointments/New/selectbank.dart
+++ lib/UI/screens/Appointments/New/selectbank.dart
@@ -3,6 +3,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:virtualQ/Services/authentication_helper.dart';
 import 'package:virtualQ/UI/Animation/fadeanimation.dart';
 import 'package:virtualQ/UI/widgets/reusable_widgets.dart';
+import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:virtualQ/utilitis/constants/api_urls.dart';
@@ -15,6 +16,7 @@ class SelectBank extends StatefulWidget {
 class _SelectBankState extends State<SelectBank> {
   final storage = new FlutterSecureStorage();
   bool _bankSelected = false;
+  Future banks;
 
   List bankData = List();
   List branchData = List();
@@ -22,7 +24,7 @@ class _SelectBankState extends State<SelectBank> {
   String selBank;
   String selBranch;
 
-  Future<String> getBanks() async {
+  Future getBanks() async {
     AuthenticationHelper().checkTokenStatus();
     String loginToken = await storage.read(key: 'accesstoken');
 
@@ -32,16 +34,19 @@ class _SelectBankState extends State<SelectBank> {
       'Authorization': 'Bearer $loginToken',
     };
     var res = await http.get(Urls.banks, headers: requestHeaders);
-    var resBody = json.decode(res.body);
-    print(resBody);
+    if (res.statusCode == 200) {
+      var resBody = json.decode(res.body);
+      print(resBody);
 
-    setState(() {
-      bankData = resBody;
-    });
-    return "Sucess";
+      setState(() {
+        bankData = resBody;
+      });
+    } else {
+      throw Exception('Failed to load Banks');
+    }
   }
 
-  Future<String> getBranch(String id) async {
+  Future getBranch(String id) async {
     AuthenticationHelper().checkTokenStatus();
     String loginToken = await storage.read(key: 'accesstoken');
 
@@ -53,158 +58,168 @@ class _SelectBankState extends State<SelectBank> {
 
     var res = await http.get(Urls.banks + '/' + id + Urls.branches,
         headers: requestHeaders);
-    var resBody = json.decode(res.body);
-    print(resBody);
-    setState(() {
-      branchData = resBody;
-      _bankSelected = false;
-    });
-    return "Sucess";
+    if (res.statusCode == 200) {
+      var resBody = json.decode(res.body);
+      print(resBody);
+      setState(() {
+        branchData = resBody;
+        _bankSelected = false;
+      });
+    } else {
+      throw Exception('Failed to load Branches');
+    }
   }
 
   @override
   void initState() {
-    getBanks();
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
+    banks = getBanks();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                'Select Your Bank',
-                style: TextStyle(
-                  color: Colors.lightBlue[900],
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(20, 50, 20, 40),
-              child: ReusableWidgets()
-                  .customImage(context, 'assets/images/bank.png'),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
-              child: ReusableWidgets().customContainer(
-                Column(
+        child: FutureBuilder(
+            future: banks,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return ListView(
                   children: [
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(25, 0, 25, 5),
-                      child: DropdownButton(
-                        isExpanded: true,
-                        autofocus: true,
-                        icon: Icon(Icons.arrow_drop_down),
-                        iconSize: 36,
-                        dropdownColor: Colors.grey[200],
-                        hint: Text(
-                          'Choose Bank',
-                          style: TextStyle(
-                            color: Colors.grey[400],
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
-                          ),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.only(top: 20),
+                      child: Text(
+                        'Select Your Bank',
+                        style: TextStyle(
+                          color: Colors.lightBlue[900],
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
                         ),
-                        value: selBank,
-                        items: bankData.map((item) {
-                          return DropdownMenuItem(
-                            child: Text(
-                              item['name'],
-                              style: TextStyle(
-                                fontSize: 15,
-                              ),
-                            ),
-                            value: item['id'].toString(),
-                          );
-                        }).toList(),
-                        onChanged: (newVal) {
-                          setState(
-                            () {
-                              selBank = newVal;
-                              _bankSelected = true;
-                            },
-                          );
-                          getBranch(selBank);
-                        },
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.fromLTRB(25, 0, 25, 5),
-                      child: _bankSelected
-                          ? Center(
-                              child: CircularProgressIndicator(),
-                            )
-                          : DropdownButton(
-                              isExpanded: true,
-                              autofocus: true,
-                              icon: Icon(Icons.arrow_drop_down),
-                              iconSize: 36,
-                              dropdownColor: Colors.grey[200],
-                              hint: Text(
-                                'Choose Branch',
-                                style: TextStyle(
-                                  color: Colors.grey[400],
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              value: selBranch,
-                              items: branchData.map((item) {
-                                return DropdownMenuItem(
-                                  child: Text(
-                                    item['name'],
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                    ),
+                      padding: EdgeInsets.fromLTRB(20, 50, 20, 40),
+                      child: ReusableWidgets()
+                          .customImage(context, 'assets/images/bank.png'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+                      child: ReusableWidgets().customContainer(
+                        Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(25, 0, 25, 5),
+                              child: DropdownButton(
+                                isExpanded: true,
+                                autofocus: true,
+                                icon: Icon(Icons.arrow_drop_down),
+                                iconSize: 36,
+                                dropdownColor: Colors.grey[200],
+                                hint: Text(
+                                  'Choose Bank',
+                                  style: TextStyle(
+                                    color: Colors.grey[400],
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  value: item['id'].toString(),
-                                );
-                              }).toList(),
-                              onChanged: (newVal) {
-                                setState(
-                                  () {
-                                    selBranch = newVal;
-                                    print(selBranch);
-                                  },
-                                );
-                              },
+                                ),
+                                value: selBank,
+                                items: bankData.map((item) {
+                                  return DropdownMenuItem(
+                                    child: Text(
+                                      item['name'],
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    value: item['id'].toString(),
+                                  );
+                                }).toList(),
+                                onChanged: (newVal) {
+                                  setState(
+                                    () {
+                                      selBank = newVal;
+                                      _bankSelected = true;
+                                    },
+                                  );
+                                  getBranch(selBank);
+                                },
+                              ),
                             ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(25, 0, 25, 5),
+                              child: _bankSelected
+                                  ? Center(
+                                      child: CircularProgressIndicator(),
+                                    )
+                                  : DropdownButton(
+                                      isExpanded: true,
+                                      autofocus: true,
+                                      icon: Icon(Icons.arrow_drop_down),
+                                      iconSize: 36,
+                                      dropdownColor: Colors.grey[200],
+                                      hint: Text(
+                                        'Choose Branch',
+                                        style: TextStyle(
+                                          color: Colors.grey[400],
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      value: selBranch,
+                                      items: branchData.map((item) {
+                                        return DropdownMenuItem(
+                                          child: Text(
+                                            item['name'],
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                            ),
+                                          ),
+                                          value: item['id'].toString(),
+                                        );
+                                      }).toList(),
+                                      onChanged: (newVal) {
+                                        setState(
+                                          () {
+                                            selBranch = newVal;
+                                            print(selBranch);
+                                          },
+                                        );
+                                      },
+                                    ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    FadeAnimation(
+                      1.2,
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+                        child: InkWell(
+                          onTap: () {
+                            if (selBank != null && selBranch != null) {
+                              Navigator.pushNamed(
+                                  context, 'tokenform/$selBranch');
+                            }
+                          },
+                          child: ReusableWidgets().customButton(
+                            context,
+                            'Next',
+                          ),
+                        ),
+                      ),
                     ),
                   ],
-                ),
-              ),
-            ),
-            FadeAnimation(
-              1.2,
-              Padding(
-                padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
-                child: InkWell(
-                  onTap: () {
-                    if (selBank != null && selBranch != null) {
-                      Navigator.pushNamed(context, 'tokenform/$selBranch');
-                    }
-                  },
-                  child: ReusableWidgets().customButton(
-                    context,
-                    'Next',
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }),
       ),
     );
   }

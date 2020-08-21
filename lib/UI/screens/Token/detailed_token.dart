871d4408409a250c6bc736b8cfa5fff7eac6e379
unknown <jithuchy777@gmail.com>
2020-08-21 15:57:17 +0530

    added token status

--- lib/UI/screens/Token/detailed_token.dart
+++ lib/UI/screens/Token/detailed_token.dart
@@ -15,12 +15,28 @@ class DetailedToken extends StatefulWidget {
 class _DetailedTokenState extends State<DetailedToken> {
   Future tokenList;
   String userName = '';
+  int id;
   String bank = '';
   String branch = '';
   String date = '';
   String service = '';
+  String counter = '';
   String token = '';
   final storage = new FlutterSecureStorage();
+  tokenStatus(String status) async {
+    await AuthenticationHelper().checkTokenStatus();
+    String loginToken = await storage.read(key: 'accesstoken');
+
+    Map<String, String> requestHeaders = {
+      'Authorization': 'Bearer $loginToken',
+    };
+    var data = {"id": id.toString(), "status": status};
+    var res =
+        await http.post(Urls.tokenUpdate, headers: requestHeaders, body: data);
+    print(res.body);
+    print(res.statusCode);
+  }
+
   Future getToken() async {
     await AuthenticationHelper().checkTokenStatus();
     String loginToken = await storage.read(key: 'accesstoken');
@@ -38,11 +54,14 @@ class _DetailedTokenState extends State<DetailedToken> {
       setState(() {
         userName = resBody[0]['username'];
         bank = resBody[0]['bank'];
+        id = resBody[0]['id'];
+        print(id);
         branch = resBody[0]['branch'];
         date = resBody[0]['token_date'];
         date = date.substring(0, date.indexOf('T'));
         service = resBody[0]['service'];
         token = resBody[0]['order_number'];
+        counter = resBody[0]['counter_number'];
       });
     } else {
       throw Exception('Failed to load Banks');
@@ -90,12 +109,15 @@ class _DetailedTokenState extends State<DetailedToken> {
                       ReusableWidgets().customText('Bank:     $bank'),
                       ReusableWidgets().customText('Branch:  $branch'),
                       ReusableWidgets().customText('Date:       $date'),
+                      ReusableWidgets().customText('Counter Nmber:  $counter'),
                       ReusableWidgets().customText('Purpose:  $service'),
                       Padding(
                         padding: const EdgeInsets.fromLTRB(20, 8, 20, 5),
                         child: RaisedButton(
                           color: Colors.blueGrey[100],
-                          onPressed: () {},
+                          onPressed: () {
+                            tokenStatus('10');
+                          },
                           child: Center(
                             child: Row(
                               children: [
@@ -115,6 +137,41 @@ class _DetailedTokenState extends State<DetailedToken> {
                                     padding: EdgeInsets.only(top: 10),
                                     child: Icon(
                                       Icons.cancel,
+                                      color: Colors.red[800],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 8, 20, 5),
+                        child: RaisedButton(
+                          color: Colors.blueGrey[100],
+                          onPressed: () {
+                            tokenStatus('15');
+                          },
+                          child: Center(
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(80, 10, 0, 5),
+                                  child: Text(
+                                    'Completed',
+                                    style: TextStyle(
+                                      fontSize: 17,
+                                      color: Colors.red[800],
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: EdgeInsets.fromLTRB(14, 10, 0, 0),
+                                    child: Icon(
+                                      Icons.assignment_turned_in,
                                       color: Colors.red[800],
                                     ),
                                   ),
